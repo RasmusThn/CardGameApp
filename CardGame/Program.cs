ffb1de5cce23cd35d46a1a3180bb771259ab0f5e
Rasmus Larsson <rasmus795@gmail.com>
2022-09-15 09:17:35 +0200

    Add project files.

---
+++ CardGame/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using CardLibrary.Cards;
+using CardLibrary.Enums;
+using System.Drawing;
+
+List<Hearts> hearts = new List<Hearts>();
+
+for (int i = 0; i <= 13; i++)
+{
+    Hearts heart = new Hearts();
+    hearts.Add(heart);
+}
+
+foreach (var heart in hearts)
+{
+    heart.Color = CardColors.Hearts;
+}
